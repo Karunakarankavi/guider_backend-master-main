9d630d82a6c5bb408f9636e835d836c06c232e57
@Karunakarankavi <karunakarankavitha3@gmail.com>
2025-04-18 17:58:31 +0530

    register complete

--- src/main/java/com/guider/controller/RegistrationController.java
+++ src/main/java/com/guider/controller/RegistrationController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +14,7 @@ import com.guider.dto.Response;
 import com.guider.entity.User;
 import com.guider.repository.UserRepository;
 import com.guider.services.UserService;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/register")
 public class RegistrationController {
